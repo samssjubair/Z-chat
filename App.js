095bac1827b2605de1775a202547917feeb93d01
Dipta Sikder <anotheramit8@gmail.com>
2021-06-03 22:20:21 +0600

    added RegisterScreen

--- App.js
+++ App.js
@@ -3,14 +3,21 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import RegisterScreen from './Screens/RegisterScreen';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-    <View style={styles.container}>
-      <Text>Hello World! I am making Z-Chat app</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Text>Hello World! I am making Z-Chat app</Text>
+        <StatusBar style="auto" />
+        <Stack.Navigator>
+          <Stack.Screen name="Register" component={RegisterScreen} />
+        </Stack.Navigator>
+      </View>
     </NavigationContainer>
   );
 }
---
+++ Screens/RegisterScreen.js
@@ -0,0 +1,75 @@
+import React, { useLayoutEffect, useState } from 'react'
+import { View, KeyboardAvoidingView, StyleSheet } from 'react-native';
+import { Button, Input, Text } from 'react-native-elements';
+// import {auth} from '../firebase';
+
+const RegisterScreen = ({ navigation }) => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [imageUrl, SetImageUrl] = useState("");
+
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerBackTitle: "Back toLogin",
+        });
+    }, [navigation]);
+
+    const register = () => {
+        auth.createUserWithEmailAndPassword(email, password)
+            .then(authUser => {
+                authUser.user.update({
+                    diaplayName: name,
+                    photoURL: imageUrl || "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
+                })
+            })
+            .catch(error => alert(error.message));
+    };
+
+    return (
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+            <Text h3 style={{ marginBottom: 50 }}>
+                Create a Z account
+            </Text>
+            <View style={styles.inputContainer}>
+                {/* Name */}
+                <Input placeholder="Full Name" autofocus type="text" value={name} onChangeText={text => setName(text)} />
+
+                {/* Email */}
+                <Input placeholder="Email" type="email" value={email} onChangeText={text => setEmail(text)} />
+
+                {/* Password */}
+                <Input placeholder="Password" autofocus type="text"
+                    secureTextEntry value={password} onChangeText={text => setPassword(text)} />
+
+                {/* ImageUrL */}
+                <Input placeholder="Profile Picture URL (optional)" type="text" value={imageUrl} onChangeText={text => SetImageUrl(text)} onSubmitEditing={register} />
+            </View>
+            <Button
+                onPress={register}
+                raised
+                title="Register"
+            />
+            <View style={{ height: 100 }} />
+        </KeyboardAvoidingView>
+    )
+}
+
+export default RegisterScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        padding: 10,
+        backgroundColor: "white",
+    },
+    button: {
+        width: 200,
+        marginTop: 10,
+    },
+    inputContainer: {
+        width: 300,
+    }
+});
