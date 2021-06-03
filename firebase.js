4afea6c771e6f6d653d67f0bdd9ae2b19012a75a
mohammadkamrulhasan <kamrul786in@gmail.com>
2021-06-03 20:43:25 +0600

    Update firebase settings

--- firebase.js
+++ firebase.js
@@ -1,3 +1,12 @@
+import firebase from 'firebase/app';
+import "firebase/auth";
+//import "firebase/database";
+import "firebase/firestore";
+//import "firebase/functions";
+//import "firebase/storage";
+
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyAomWFfvrWaeOXRrrjIUbO1rW0NMcAHd2Y",
     authDomain: "z-chat-cfb4a.firebaseapp.com",
@@ -6,3 +15,21 @@ const firebaseConfig = {
     messagingSenderId: "498711247537",
     appId: "1:498711247537:web:cfd83e60d83c7473378090"
   };
+
+let app;
+
+if(firebase.apps.length === 0){
+
+  app = firebase.initializeApp(firebaseConfig)
+
+}else{
+  app = firebase.app();
+
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+
+export {db , auth };
+
+  
