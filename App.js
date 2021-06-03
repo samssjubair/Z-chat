b5fdb8ecb9ded1462dbcd11c53d994b68d118e21
mohammadkamrulhasan <kamrul786in@gmail.com>
2021-06-03 14:52:16 +0600

    Update firebase config

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+     <View style={styles.container}>
       <StatusBar style="auto" />
+      <Text>Z-chat is comming !</Text>
+      
     </View>
+    </NavigationContainer>
+    
   );
 }
 
---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAomWFfvrWaeOXRrrjIUbO1rW0NMcAHd2Y",
+    authDomain: "z-chat-cfb4a.firebaseapp.com",
+    projectId: "z-chat-cfb4a",
+    storageBucket: "z-chat-cfb4a.appspot.com",
+    messagingSenderId: "498711247537",
+    appId: "1:498711247537:web:cfd83e60d83c7473378090"
+  };
