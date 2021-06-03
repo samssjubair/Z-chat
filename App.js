5a03f9ef6242896e444222168bc99a0dd05e4cb3
mohammadkamrulhasan <kamrul786in@gmail.com>
2021-06-03 21:40:47 +0600

    Update App file

--- App.js
+++ App.js
@@ -4,14 +4,20 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
+const stack=createStackNavigator();
+
+const globalScreenOptions={
+  headerStyle: { backgroundColor: "#2C6BED" },
+  headerTitleStyle: {color: "white" },
+  headerTinColor: "white",
+};
+
 export default function App() {
   return (
     <NavigationContainer>
-     <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text>Z-chat is comming !</Text>
-      
-    </View>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+      <stack.Screen name="Add Chat" component={} />
+      </Stack.Navigator>     
     </NavigationContainer>
     
   );
