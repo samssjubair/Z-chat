5f007d22213cc12914fb219334aab573e35aed97
Dipta Sikder <anotheramit8@gmail.com>
2021-06-03 16:59:35 +0600

    test commit

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
+    <NavigationContainer>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World! I am making Z-Chat app</Text>
       <StatusBar style="auto" />
     </View>
+    </NavigationContainer>
   );
 }
 
